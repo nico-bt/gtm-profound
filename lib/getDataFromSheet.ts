ffb2e1c28f5647eeebf1ac371911bba6ee9b400e
Nico-Battaglia <nico-battaglia@hotmail.com>
2026-01-30 14:46:37 -0300

    Refactor: optimize data fetching in parallel

--- lib/getDataFromSheet.ts
+++ lib/getDataFromSheet.ts
@@ -22,15 +22,11 @@ const DOC_URL =
 
 // OBS: This function could be more generic by accepting sheet names and URL as parameters
 // But for simplicity for this challenge is hardcoded
-// *****************************************************************************************
 export const getDataFromSheet = async () => {
-  const csvReps = await fetch(`${DOC_URL}/gviz/tq?tqx=out:csv&sheet=Reps `).then((res) =>
-    res.text(),
-  )
-
-  const csvAccounts = await fetch(`${DOC_URL}/gviz/tq?tqx=out:csv&sheet=Accounts `).then((res) =>
-    res.text(),
-  )
+  const [csvReps, csvAccounts] = await Promise.all([
+    fetch(`${DOC_URL}/gviz/tq?tqx=out:csv&sheet=Reps`).then((res) => res.text()),
+    fetch(`${DOC_URL}/gviz/tq?tqx=out:csv&sheet=Accounts`).then((res) => res.text()),
+  ])
 
   // console.log({ csvReps, csvAccounts })
 
